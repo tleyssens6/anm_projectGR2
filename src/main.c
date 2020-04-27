4f9eb0418b2fdc860149aaae5d2af755c88a78b3
louisals <63903968+louisals@users.noreply.github.com>
2020-04-27 11:45:02 +0200

    Update main.c

--- src/main.c
+++ src/main.c
@@ -43,11 +43,16 @@ void dam_break(){
 
 
 	// SPH parameters
-	int n_per_dim = 51; // number of particles per dimension
-	double kh = sqrt(21) * 2 * l / n_per_dim; // kernel width to ensure 21 particles in the neighborhood
+	double N_x1 = 30;
+	double N_y1 = 30;
+	double N_x2 = 45;
+	double N_y2 = 20;
+	double kh = sqrt(21) * 2 * l / 25;
+
+	//int n_per_dim = 60; // number of particles per dimension
+	//double kh = sqrt(21) * 2 * l / n_per_dim; // kernel width to ensure 21 particles in the neighborhood
 	int n_iter = (int)(T / dt); // number of iterations to perform
 	Kernel kernel = Cubic; // kernel choice
-	double interface_threshold = 1.2;//1.5; // If ||n_i|| > threshold => particle i belongs to interface (first detection approach)
 	
     int T_verlet = 4;
     double L_verlet = 1.1*(2*3.4*(double)T_verlet*dt); // 􏱉L = nu*􏱑(2Vmax· C·dt)
@@ -66,27 +71,51 @@ void dam_break(){
 	double T_anim = 10; // duration of animation
 	double dt_anim = T_anim / n_iter; // time step of animation
 
-	// Initialize particles on a square
-	int n_p = squared(n_per_dim); // total number of particles
-	double h = 2 * l / (n_per_dim - 1); // step between neighboring particles
-	double m = rho_0 * h*h;
+	
+	int n_p = N_x1 * N_y1 + N_x2 * N_y2;
+	double h_x1 = (2 * l - 0.01) / (N_x1 - 1);
+	double h_y1 = (2 * l - 0.01) / (N_y1 - 1);
+	double h_x2 = (3 * l - 0.01) / (N_x2 - 1);
+	double h_y2 = (0.5 * l ) / (N_y2 - 1);
+
+	//int n_p = squared(n_per_dim);// +N_x * N_y; // total number of particles
+	printf("nombre de point %d", n_p);
+	//double h_x = (2 * l -0.01) / (n_per_dim - N_x2 - 1); // step between neighboring particles
+	//double h_y= 2 * l / (n_per_dim - 1);
+	//double m1 = rho_0 * h_x*h_y;
+	double m1 = rho_0 * h_x1 * h_y1;
+	double m2 = rho_0 * h_x2 * h_y2;
 	Particle** particles = (Particle**)malloc(n_p * sizeof(Particle*));
 	Particle_derivatives** particles_derivatives = malloc(n_p * sizeof(Particle_derivatives*));
 	Residual** residuals = malloc(n_p * sizeof(Residual*));
-	for (int i = 0; i < n_per_dim; i++) {
-		for (int j = 0; j < n_per_dim; j++) {
-			int index = i * n_per_dim + j;
-			xy *pos = xy_new(-2*l + i * h,  j * h);
-			xy *v = xy_new(0.0, 0.0); // initial velocity = 0
-			// Insert initial condition on velocity here
-			particles[index] = Particle_new(index, m, pos, v, rho_0, mu, c_0, gamma, sigma);
+
+	for (int i = 0; i < N_y1; i++) {
+		for (int j = 0; j < N_x1; j++) {
+			int index = i * (N_x1)+j;
+			xy* pos;
+			xy* v;
+			pos = xy_new(-2 * l + i * h_x1, j * h_y1);
+			v = xy_new(0.0, 0.0); // initial velocity = 0
+			particles[index] = Particle_new(index, m1, pos, v, rho_0, mu, c_0, gamma, sigma);
+			particles_derivatives[index] = Particle_derivatives_new(index);
+			residuals[index] = Residual_new();
+		}
+	}
+	int index1 = N_x1 * N_y1;
+	for (int i = 0; i < N_y2; i++) {
+		for (int j = 0; j < N_x2; j++) {
+			int index = index1 + i * (N_x2)+j;
+			xy* pos;
+			xy* v;
+			pos = xy_new((j) * h_x2+0.005,-l+ i * h_y2);
+			v = xy_new(0.0, 0.0); // initial velocity = 0
+			particles[index] = Particle_new(index, m2, pos, v, rho_0, mu, c_0, gamma, sigma);
 			particles_derivatives[index] = Particle_derivatives_new(index);
 			residuals[index] = Residual_new();
 		}
 	}
 	// Setup grid
 	Grid *grid = Grid_new(-3*l, 4*l, -H, H, kh);
-    //Grid *grid = Grid_new(-L, L, -H, H, kh);
 	// Setup BOUNDARY
 	double lb = 0.420;
 	double hb = 0.440;
@@ -108,6 +137,9 @@ void dam_break(){
 	Setup_free(setup);
 	Animation_free(animation);
 }
+/// with 2 liquids
+
+
 // Evolution of a 2D circle with non-zero initial velocities (no surface tension force)
 // Test case from "Simulating Free Surface Flows with SPH", Monaghan (1994)
 
